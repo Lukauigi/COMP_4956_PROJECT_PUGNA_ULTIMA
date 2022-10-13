a74f6d315aaedef89514afc788f76e185a456bc2
Jun Earl Solomon <junearlsolomon14@gmail.com>
2022-10-13 07:37:56 -0700

    done some testing

---
+++ Assets/Scripts/Capabilities/Move.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] private InputController input = null;
+    [SerializeField, Range(0f, 100f)] private float maxSpeed = 4f;
+    [SerializeField, Range(0f, 100f)] private float maxAcceleration = 35f;
+    [SerializeField, Range(0f, 100f)] private float maxAirAcceleration = 20f;
+
+    private Vector2 direction;
+    private Vector2 desiredVelocity;
+    private Vector2 velocity;
+    private Rigidbody2D body;
+    private Ground ground;
+
+    private float maxSpeedChange;
+    private float acceleration;
+    private bool onGround;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// makes sure we can make an instance of it in the menu, just makes things easier xd
+[CreateAssetMenu(fileName ="PlayerController", menuName ="InputController/PlayerController")]
+public class PlayerController : InputController
+{
+    public override float RetrieveMoveInput()
+    {
+        return Input.GetAxisRaw("Horizontal");
+    }
+
+    public override bool RetrieveJumpInput()
+    {
+        return Input.GetButton("Jump");
+    }
+
+}
