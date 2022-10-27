4b22db1cf0a4b4022b440519963c68767f38be9d
Sean <xzhu32@my.bcit.ca>
2022-10-27 01:32:58 -0700

    Added the button and make it able to navigate from starting screen to main screen.

---
+++ Assets/Scripts/ui/StartButton.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("Main Screen");
+    }
+}
