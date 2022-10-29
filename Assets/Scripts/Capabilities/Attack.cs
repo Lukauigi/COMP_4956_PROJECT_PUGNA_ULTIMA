3c32dbe8b50eb283d1e3eeabb84feeff7689100a
Jun Earl Solomon <junearlsolomon14@gmail.com>
2022-10-29 00:06:16 -0700

    fixed collider issues in Attack.cs

--- Assets/Scripts/Capabilities/Attack.cs
+++ Assets/Scripts/Capabilities/Attack.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Attack : MonoBehaviour //change to NetworkBehaviour
 {
 
-    public Collider[] attackHitboxes;
+    public Collider2D[] attackHitboxes;
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +20,15 @@ public class Attack : MonoBehaviour //change to NetworkBehaviour
             LaunchAttack(attackHitboxes[0]);
     }
 
-    private void LaunchAttack(Collider col)
+    private void LaunchAttack(Collider2D col)
     {
-        Collider[] cols = Physics.OverlapBox(col.bounds.center, col.bounds.extents, col.transform.rotation, LayerMask.GetMask("Hitbox"));
-        foreach (Collider c in cols)
+        Collider2D[] cols = Physics2D.OverlapBoxAll(col.bounds.center, col.bounds.extents, col.transform.rotation.x, LayerMask.GetMask("Hitbox"));
+        foreach (Collider2D c in cols)
         {
+            //Debug.Log(c.name);
+            if (c.transform.parent.parent == transform) // Check if attack hitbox is colliding with the player that used the attack
+                continue;                               // If so do not register a hit and continue foreach loop
             Debug.Log(c.name);
-          /*  if (c.transform.parent.parent == transform) // Check if attack hitbox is colliding with the player that used the attack
-                continue;   */                            // If so do not register a hit and continue foreach loop
         }
        
     }
