c810486da70ac91d99a961a0c38bed5ab7076221
Roswell Doria <rdoria6@my.bcit.ca>
2022-10-29 00:57:55 -0700

    Updated Comments for PlayerItemController

--- Assets/Scripts/Network/PlayerItem/PlayerItemController.cs
+++ Assets/Scripts/Network/PlayerItem/PlayerItemController.cs
@@ -4,6 +4,14 @@ using Fusion;
 using UnityEngine.UI;
 using System.Threading;
 
+/// <summary>
+/// Author: Roswell Doria
+/// Date: 2022-10-29
+/// 
+/// Network Controller for PlayerItem Prefab. 
+/// This is responsible for functionality of the Prefab object across the network.
+///
+/// </summary>
 public class PlayerItemController : NetworkBehaviour
 {
     protected NetworkCharacterControllerPrototype _ncc;
@@ -14,16 +22,36 @@ public class PlayerItemController : NetworkBehaviour
     [SerializeField] private Color[] Colors;
     [SerializeField] private int selected;
 
+    /// <summary>
+    /// Author: Roswell Doria
+    /// Date: 2022-10-29
+    /// 
+    /// Method calls before a network playerItem is spawned.
+    ///
+    /// </summary>
     public void Awake()
     {
         CacheComponents();
     }
 
+    /// <summary>
+    /// Author: Roswell Doria
+    /// Date: 2022-10-29
+    ///
+    /// Method calls when network playerItem is spawned.
+    ///
+    /// </summary>
     public override void Spawned()
     {
         CacheComponents();
     }
 
+    /// <summary>
+    /// Author: Roswell Doria
+    /// 
+    /// Helper function to Initilize components.
+    ///
+    /// </summary>
     private void CacheComponents()
     {
         if (!_playerItem) _playerItem = GetComponent<PlayerItem>();
@@ -34,6 +62,13 @@ public class PlayerItemController : NetworkBehaviour
         //if(!_color) _color = GetComponent<NetworkColor>();
     }
 
+    /// <summary>
+    /// Author: Roswell Doria
+    /// Date: 2022-10-29
+    /// 
+    /// Updates the network of a local client's changes to their ownership of this networked object.
+    ///
+    /// </summary>
     public override void FixedUpdateNetwork()
     {
 
@@ -43,18 +78,41 @@ public class PlayerItemController : NetworkBehaviour
         }
     }
 
+    /// <summary>
+    /// Author: Roswell Doria
+    /// Date: 2022-10-29
+    ///
+    /// Remote procedure call used to update the avatar of the input authortive object and notify
+    /// all other connected clients of this update.
+    ///
+    /// </summary>
+    /// <param name="selectedColor"></param>
     [Rpc(sources: RpcSources.InputAuthority, RpcTargets.All)]
     public void RPC_ChangeAvatar(Color selectedColor)
     {
         Avatar.color = selectedColor;
     }
 
+    /// <summary>
+    /// Author: Roswell Doria
+    /// Date: 2022-10-29
+    ///
+    /// Remote procedure call used to update the selected character of the input authoritve object and notify
+    /// all other connected clients of this update. The intended use of this method is to be attached to a next button.
+    /// </summary>
     [Rpc(sources: RpcSources.InputAuthority, RpcTargets.All)]
     public void RPC_OnNextBtnClick()
     {
         selected = (selected + 1) % Colors.Length;
     }
 
+    /// <summary>
+    /// Author: Roswell Doria
+    /// Date: 2022-10-29
+    ///
+    /// Remote procedure call used to update the selected character of the input authoritve object and notify
+    /// all other connected clients of this update. The intended use of this method is to be attached to a previous button.
+    /// </summary>
     [Rpc(sources: RpcSources.InputAuthority, RpcTargets.All)]
     public void RPC_OnPrevBtnClick()
     {
