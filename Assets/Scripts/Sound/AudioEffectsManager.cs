e314923b2068b70f25d35582c21fa3d054f2e2b3
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2022-11-08 12:33:59 -0800

    Add documentation and comments

--- Assets/Scripts/Sound/AudioEffectsManager.cs
+++ Assets/Scripts/Sound/AudioEffectsManager.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// 
+/// An audio sound effects manager which manages all sound effects in a scene.
+/// Author(s): Lukasz Bednarek
+/// Date: November 11, 2022
+/// Source(s): N/A
+/// Remarks:
+/// Change History: November 11 - Lukasz Bednarek
+/// Wrote documentation and comments.
 /// </summary>
 public class AudioEffectsManager : MonoBehaviour
 {
@@ -18,32 +24,44 @@ public class AudioEffectsManager : MonoBehaviour
     /// </summary>
     void Awake()
     {
+        /*  
+        This process is done to have an audio effect play through a scene transition and have the new audio effects manager for the next scene.
+         */
+
+        //If no instances, create the first instance of an audio effects manager.
         if (_instanceCount == 0)
         {
             Instance1 = this;
             DontDestroyOnLoad(gameObject);
             _instanceCount++;
         }
+
+        //If 1 instance, create another instance of an audio effects manager.
         else if (_instanceCount == 1)
         {
             Instance2 = this;
             DontDestroyOnLoad(gameObject);
             _instanceCount++;
         }
+
+        //If 2 instances, destroy the old audio effects manager and make a new one in its place and point to the other audio effects manager as the old manager.
         else if (_instanceCount == _maxInstanceCounter)
         {
+            //replace instance 1 with new instance.
             if (_oldInstanceNum == 1)
-            {
-                Destroy(Instance1.gameObject);
-                ChangeOldInstanceIndicator();
+            { 
+                Destroy(Instance1.gameObject); 
+                ChangeOldInstanceIndicator(); //change count to 2 to indicate instance 2 is the older manager.
                 
                 Instance1 = this;
                 DontDestroyOnLoad(gameObject);
             }
+
+            //replace instance 2 with new instance.
             else if (_oldInstanceNum == 2)
             {
                 Destroy(Instance2.gameObject);
-                ChangeOldInstanceIndicator();
+                ChangeOldInstanceIndicator(); //change count to 1 to indicate instance 1 is the older manager.
 
                 Instance2 = this;
                 DontDestroyOnLoad(gameObject);
