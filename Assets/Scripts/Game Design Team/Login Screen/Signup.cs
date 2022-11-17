6047b36b1d387cd1418293807b87464b1abe2587
Jason Cheung <jason.cheung@live.ca>
2022-11-17 15:03:57 -0800

    comment namespace bug

--- Assets/Scripts/Game Design Team/Login Screen/Signup.cs
+++ Assets/Scripts/Game Design Team/Login Screen/Signup.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEditor.Events;
+//using UnityEditor.Events;
 using UnityEngine.Events;
 
 /// <summary>
